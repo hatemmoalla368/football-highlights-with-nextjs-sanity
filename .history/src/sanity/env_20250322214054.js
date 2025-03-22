9f6e01fe52e056d5599d27d4721cab9729b64405
unknown <hatemmoalla368@gmail.com>
2025-03-22 21:41:24 +0100

    Commit new version

---
+++ .history/src/sanity/env_20250322214054.js
@@ -0,0 +1,8 @@
+
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-03-13'
+
+export const dataset = "production" ;
+export const projectId = "cll2lcsk";
+export const token = "skpua6kyyV3OUYKYkhtlMO8SMJ1clQKIyOKlMVG8XRGLr9FYfVYZplxeMvXnW7jaqdIpAvXy9DMeTEJPBcf5IscKVE0yLwiCbYqZUydvsSY8LkA2dFLWJf2QGsD05liKBWzYeU2EKFKVa0ESLTONZB4lr5VPl4EM0jyxqGDNLzutWSletkbh";
+
